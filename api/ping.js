2302f4e423adb024c0ff3c8c64dab03a389ca60c
photoglowai-stack <photoglow.ai@gmail.com>
2025-10-22 16:45:54 +0200

    Delete api/ping.js

--- api/ping.js
+++
@@ -1,6 +0,0 @@
-// /api/ping.js
-module.exports = (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  if (req.method === "OPTIONS") return res.status(204).end();
-  return res.status(200).json({ ok: true, msg: "pong" });
-};
